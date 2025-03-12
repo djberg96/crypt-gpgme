be4ebcc480be63f4bb482c9446712261c58d42ab
Daniel Berger <78529+djberg96@users.noreply.github.com>
2025-03-12 08:38:57 -0400

    Add key and revocation key structs.

--- lib/crypt/gpgme/structs.rb
+++ lib/crypt/gpgme/structs.rb
@@ -24,6 +24,44 @@ module Crypt
         )
       end
 
+      # gpgme_key_t
+      class Key < FFI::Struct
+        layout(
+          :_refs, :uint,
+          :revoked, :bool,
+          :expired, :bool,
+          :disabled, :bool,
+          :invalid, :bool,
+          :can_encrypt, :bool,
+          :can_sign, :bool,
+          :can_certify, :bool,
+          :secret, :bool,
+          :can_authenticate, :bool,
+          :is_qualified, :bool,
+          :has_encrypt, :bool,
+          :has_sign, :bool,
+          :has_certify, :bool,
+          :has_authenticate, :bool,
+          :_unused, :uint,
+          :origin, :int,
+          :protocol, :uint,
+          :issuer_serial, :string,
+          :issuer_name, :string,
+          :chain_id, :string,
+          :owner_trust, :pointer,
+          :subkeys, :pointer,
+          :uids, :pointer,
+          :_last_subkey, :pointer,
+          :_last_uid, :pointer,
+          :keylist_mode, :pointer,
+          :fpr, :string,
+          :last_update, :ulong,
+          :revocation_keys, :pointer,
+          :_last_revkey, :pointer
+        )
+      end
+
+      # gpgme_subkey_t
       class Subkey < FFI::Struct
         layout(
           :next, :pointer,
@@ -55,6 +93,33 @@ module Crypt
           :curve, :string,
           :keygrip, :string,
           :v5fpr, :string
+        )
+      end
+
+=begin
+      class UserId < FFI::Struct
+        layout(
+        )
+      end
+
+      class TofuInfo < FFI::Struct
+        layout(
+        )
+      end
+
+      class KeySig < FFI::Struct
+        layout(
+        )
+      end
+=end
+
+      class RevocationKey < FFI::Struct
+        layout(
+          :next, :pointer,
+          :pubkey_algo, :int,
+          :fpr, :string,
+          :key_class, :uint,
+          :sensitive, :uint
         )
       end
     end
