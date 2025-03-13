3ca25b8281f24ed9e5bbfc1d957852cca48fb96a
Daniel Berger <78529+djberg96@users.noreply.github.com>
2025-03-13 10:44:59 -0400

    Update specs for Mac using updated mkmf-lite option.

--- spec/crypt_gpgme_spec.rb
+++ spec/crypt_gpgme_spec.rb
@@ -59,29 +59,31 @@ RSpec.describe Crypt::GPGME do
     end
 
     let(:dummy){ Class.new{ extend Mkmf::Lite } }
+    let(:header) { 'gpgme.h' }
+    let(:path){ RbConfig::CONFIG['host_os'] =~ /darwin/i ? '/opt/homebrew/include' : nil }
 
     example 'engine_info is the expected size' do
-      expect(Crypt::GPGME::Structs::EngineInfo.size).to eq(dummy.check_sizeof('struct _gpgme_engine_info', 'gpgme.h'))
+      expect(Crypt::GPGME::Structs::EngineInfo.size).to eq(dummy.check_sizeof('struct _gpgme_engine_info', header, path))
     end
 
     example 'key is the expected size' do
-      expect(Crypt::GPGME::Structs::Key.size).to eq(dummy.check_sizeof('struct _gpgme_key', 'gpgme.h'))
+      expect(Crypt::GPGME::Structs::Key.size).to eq(dummy.check_sizeof('struct _gpgme_key', header, path))
     end
 
     example 'subkey is the expected size' do
-      expect(Crypt::GPGME::Structs::Subkey.size).to eq(dummy.check_sizeof('struct _gpgme_subkey', 'gpgme.h'))
+      expect(Crypt::GPGME::Structs::Subkey.size).to eq(dummy.check_sizeof('struct _gpgme_subkey', header, path))
     end
 
     example 'keysig is the expected size' do
-      expect(Crypt::GPGME::Structs::KeySig.size).to eq(dummy.check_sizeof('struct _gpgme_key_sig', 'gpgme.h'))
+      expect(Crypt::GPGME::Structs::KeySig.size).to eq(dummy.check_sizeof('struct _gpgme_key_sig', header, path))
     end
 
     example 'userid is the expected size' do
-      expect(Crypt::GPGME::Structs::UserId.size).to eq(dummy.check_sizeof('struct _gpgme_user_id', 'gpgme.h'))
+      expect(Crypt::GPGME::Structs::UserId.size).to eq(dummy.check_sizeof('struct _gpgme_user_id', header, path))
     end
 
     example 'tofuinfo is the expected size' do
-      expect(Crypt::GPGME::Structs::TofuInfo.size).to eq(dummy.check_sizeof('struct _gpgme_tofu_info', 'gpgme.h'))
+      expect(Crypt::GPGME::Structs::TofuInfo.size).to eq(dummy.check_sizeof('struct _gpgme_tofu_info', header, path))
     end
   end
 end
