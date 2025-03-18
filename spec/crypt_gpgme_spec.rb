7f0821d04a6bf7c2a7713696f2847192171cec98
Daniel Berger <78529+djberg96@users.noreply.github.com>
2025-03-18 06:51:29 -0400

    More armor specs.

--- spec/crypt_gpgme_spec.rb
+++ spec/crypt_gpgme_spec.rb
@@ -60,6 +60,17 @@ RSpec.describe Crypt::GPGME do
       expect(subject).to respond_to(:armor?)
       expect(subject.armor?).to be_boolean
     end
+
+    example 'armor= basic functionality' do
+      expect(subject).to respond_to(:armor=)
+      expect(subject.armor=true).to be_boolean
+    end
+
+    example 'armor? returns expected value' do
+      expect(subject.armor?).to be(false)
+      subject.armor=true
+      expect(subject.armor?).to be(true)
+    end
   end
 
   context 'FFI' do
