2022f63e87c4fc8255e4703fb3315b791137049c
Daniel Berger <78529+djberg96@users.noreply.github.com>
2025-06-30 10:19:47 -0400

    Add crypto functions.

--- lib/crypt/gpgme/functions.rb
+++ lib/crypt/gpgme/functions.rb
@@ -25,6 +25,16 @@ module Crypt
       # attach_function :gpgme_data_tell, [:gpgme_data_t], :off_t
       attach_function :gpgme_data_release_and_get_mem, [:gpgme_data_t, :pointer], :pointer
 
+      # Crypto operations
+      attach_function :gpgme_op_encrypt, [Structs::Context, :pointer, :uint, :gpgme_data_t, :gpgme_data_t], :gpgme_error_t
+      attach_function :gpgme_op_encrypt_start, [Structs::Context, :pointer, :uint, :gpgme_data_t, :gpgme_data_t], :gpgme_error_t
+      attach_function :gpgme_op_decrypt, [Structs::Context, :gpgme_data_t, :gpgme_data_t], :gpgme_error_t
+      attach_function :gpgme_op_decrypt_start, [Structs::Context, :gpgme_data_t, :gpgme_data_t], :gpgme_error_t
+      # attach_function :gpgme_op_sign_encrypt, [Structs::Context, :pointer, :uint, :gpgme_data_t, :gpgme_data_t], :gpgme_error_t
+      # attach_function :gpgme_op_sign_encrypt_start, [Structs::Context, :pointer, :uint, :gpgme_data_t, :gpgme_data_t], :gpgme_error_t
+      attach_function :gpgme_op_verify, [Structs::Context, :gpgme_data_t, :gpgme_data_t, :gpgme_data_t], :gpgme_error_t
+      attach_function :gpgme_op_verify_start, [Structs::Context, :gpgme_data_t, :gpgme_data_t, :gpgme_data_t], :gpgme_error_t
+
       attach_function :gpgme_check_version, [:string], :string
       attach_function :gpgme_ctx_get_engine_info, [Structs::Context], Structs::EngineInfo
       attach_function :gpgme_ctx_set_engine_info, [Structs::Context, :uint, :string, :string], :int
