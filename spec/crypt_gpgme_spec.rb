851409dc45823c24ca023187c800d5fbf754690f
Daniel Berger <78529+djberg96@users.noreply.github.com>
2025-10-20 00:30:42 -0400

    Move Algorithm specs into their own file.

--- spec/crypt_gpgme_spec.rb
+++ spec/crypt_gpgme_spec.rb
@@ -12,30 +12,6 @@ RSpec.describe Crypt::GPGME do
     expect(Crypt::GPGME::VERSION).to eq('0.1.0')
   end
 
-  context Crypt::GPGME::Algorithm do
-    example 'pubkey_algorithm_name basic functionality' do
-      expect(described_class).to respond_to(:pubkey_algorithm_name)
-      expect(described_class.pubkey_algorithm_name(1)).to be_a(String)
-    end
-
-    example 'pubkey_algorithm_name returns expected value' do
-      expect(described_class.pubkey_algorithm_name(Crypt::GPGME::GPGME_PK_DSA)).to eq('DSA')
-      expect(described_class.pubkey_algorithm_name(Crypt::GPGME::GPGME_PK_RSA)).to eq('RSA')
-      expect(described_class.pubkey_algorithm_name(9999999)).to be_nil
-    end
-
-    example 'hash_algorithm_name basic functionality' do
-      expect(described_class).to respond_to(:hash_algorithm_name)
-      expect(described_class.pubkey_algorithm_name(1)).to be_a(String)
-    end
-
-    example 'hash_algorithm_name returns expected value' do
-      expect(described_class.hash_algorithm_name(Crypt::GPGME::GPGME_MD_MD5)).to eq('MD5')
-      expect(described_class.hash_algorithm_name(Crypt::GPGME::GPGME_MD_SHA256)).to eq('SHA256')
-      expect(described_class.hash_algorithm_name(9999999)).to be_nil
-    end
-  end
-
   context 'FFI' do
     before(:context) do
       require 'mkmf-lite'
