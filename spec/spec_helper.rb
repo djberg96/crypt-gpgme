cfbcba08763b39791ea05b40ef49ad4ca16dbd86
Daniel Berger <78529+djberg96@users.noreply.github.com>
2025-10-21 07:05:52 -0400

    Add shared context.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,12 @@ require 'rspec'
 require 'rspec_boolean'
 require 'fakefs/spec_helpers'
 require 'crypt/gpgme'
+require 'tmpdir'
+
+# Load all support files
+Dir[File.expand_path('support/**/*.rb', __dir__)].each { |f| require f }
 
 RSpec.configure do |config|
   config.include FakeFS::SpecHelpers, fakefs: true
+  config.include_context 'with temporary gpg home', :tempfs
 end
