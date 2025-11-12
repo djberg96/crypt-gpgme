197549e45ad1a3af7125684fd36f163da67d4af1
Daniel Berger <78529+djberg96@users.noreply.github.com>
2025-11-11 20:10:27 -0500

    Update to_hash method for Key struct.

--- lib/crypt/gpgme/structs.rb
+++ lib/crypt/gpgme/structs.rb
@@ -305,23 +305,29 @@ module Crypt
           uid = self[:uids]
           subkey = self[:subkeys]
 
-          uid_array << uid
-          subkey_array << subkey
-
-          loop do
-            uid = Crypt::GPGME::Structs::UserId.new(uid[:next])
-            break if uid.null?
+          if !uid.null?
             uid_array << uid
+
+            loop do
+              uid = Crypt::GPGME::Structs::UserId.new(uid[:next])
+              break if uid.null?
+              uid_array << uid
+            end
+
+            hash[:uids] = uid_array.map(&:to_hash)
           end
 
-          loop do
-            subkey = Crypt::GPGME::Structs::Subkey.new(subkey[:next])
-            break if subkey.null?
+          if !subkey.null?
             subkey_array << subkey
-          end
 
-          hash[:uids] = uid_array.map(&:to_hash)
-          hash[:subkeys] = subkey_array.map(&:to_hash)
+            loop do
+              subkey = Crypt::GPGME::Structs::Subkey.new(subkey[:next])
+              break if subkey.null?
+              subkey_array << subkey
+            end
+
+            hash[:subkeys] = subkey_array.map(&:to_hash)
+          end
 
           hash
         end
